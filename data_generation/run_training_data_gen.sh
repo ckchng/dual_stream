fc7506e08deeef51d4eabf8a5f7822ac97f281d0
ckchng <chngcheekheng@gmail.com>
2026-04-10 17:18:08 -0700

    m

--- data_generation/run_training_data_gen.sh
+++ data_generation/run_training_data_gen.sh
@@ -11,12 +11,12 @@ set -euo pipefail
 
 # PYTHON=/home/ckchng/conda_env/pose_estimation/bin/python
 PYTHON=/hpcfs/users/a1775493/ck/conda_env/dual_stream/bin/python
-SCRIPT="/hpcfs/users/a1775493/ck/dual_stream/data_generation/training_data_gen_for_RT.py"
+SCRIPT="/hpcfs/users/a1775493/ck/SDA_OTE/dual_stream/data_generation/training_data_gen_for_RT.py"
 
 # ── IO ─────────────────────────────────────────────────────────────────────────
 # IMG_DIR="/home/ckchng/Documents/SDA_ODA/LMA_data/background_patches_with_new_model"
-IMG_DIR=/hpcfs/users/a1775493/ck/dual_stream/data/bg_data/
-OUTPUT_DIR="/hpcfs/users/a1775493/ck/dual_stream/data/tmp/"
+IMG_DIR=/hpcfs/users/a1775493/ck/SDA_OTE/dual_stream/data/bg_data/
+OUTPUT_DIR="/hpcfs/users/a1775493/ck/SDA_OTE/dual_stream/data/tmp/"
 
 # ── Range selection ────────────────────────────────────────────────────────────
 STARTING_ID=100
